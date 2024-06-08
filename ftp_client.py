4ea0bed033f3887f398998f1a1d9dde3df7a7eab
chrisPQ <114256638+chrisPQ@users.noreply.github.com>
2024-06-07 20:01:25 -0700

    Hotfix for password

---
+++ ftp_client.py
@@ -0,0 +1,134 @@
+import os
+import asyncio
+import socket
+from threading import Thread
+
+CHUNK = 100
+
+EOF = "EOF"
+
+IP, DPORT = 'localhost', 8080
+
+os.chdir('myfiles')
+
+def to_hex(number):
+    # Verify our assumption: error is printed and program exists if assumption is violated
+    assert number <= 0xffffffff, "Number too large"
+    return "{:08x}".format(number)
+
+
+async def recv_message(reader: asyncio.StreamReader):
+    full_data = await reader.read(CHUNK)
+    return full_data.decode()
+
+
+async def connect(i):
+    # Configure a socket object to use IPv4 and TCP
+    reader, writer = await asyncio.open_connection(IP, DPORT)
+    correctPassword = False
+    # TODO: receive the introduction message by implementing `recv_intro_message` above.
+    while True:
+        intro = await recv_message(reader)
+        print(intro)
+
+        passIn = input(" ")
+
+        # Send the filename to the server
+        await send_long_message(writer, passIn)
+
+        # Receive a response from the server
+        intro = await recv_message(reader)
+        if intro == "Incorrect \n":
+
+            print(intro)
+        elif intro.startswith("Too"):
+            print(intro)
+            writer.close()
+            await writer.wait_closed()
+            break
+        else:
+            print(intro)
+            break
+
+    while True:
+        if intro.startswith("Too"):
+            break
+
+        command = input("Command to send: ")
+
+        if command.startswith("list"):
+            await send_long_message(writer, command)
+            data = await recv_message(reader)
+            print(data)
+        elif command.startswith("put"):
+            if os.path.exists(command[4:]):
+                await send_long_message(writer, command)
+                with open(command[4:], 'rb') as f:
+                    while sendData := f.read(CHUNK):
+                        writer.write(sendData)
+                        await writer.drain()
+                f.close()
+                writer.write(EOF.encode())
+                await writer.drain()
+                print(await recv_message(reader))
+            else:
+                print("file does not exist")
+        elif command.startswith("remove"):
+            await send_long_message(writer, command)
+            print(await recv_message(reader))
+        elif command.startswith("get"):
+            await send_long_message(writer, command)
+            server_response = await recv_message(reader)
+            if server_response == "ACK":
+                print("file exists")
+                with open(command[4:], 'wb') as f:
+                    while True:
+                        dataChunk = await reader.read(CHUNK)
+
+                        if not dataChunk or dataChunk.endswith(EOF.encode()):
+                            f.write(dataChunk[:-3])
+                            break
+                        f.write(dataChunk)
+                f.close()
+            else:
+                print("file doesn't exist")
+        elif command.startswith("close"):
+            writer.close()
+            await writer.wait_closed()
+            break
+        else:
+            await send_long_message(writer, command)
+            returnMessage = await recv_message(reader)
+            print(returnMessage[4:])
+    return
+
+
+
+async def send_long_message(writer: asyncio.StreamWriter, data):
+    # TODO: Send the length of the message: this should be 8 total hexadecimal digits
+    #       This means that ffffffff hex -> 4294967295 dec
+    #       is the maximum message length that we can send with this method!
+    #       hint: you may use the helper function `to_hex`. Don't forget to encode before sending!
+
+    writer.write(to_hex(len(data)).encode())
+    writer.write(data.encode())
+
+    await writer.drain()
+
+
+
+async def main(tosend):
+    tasks = []
+    for i in range(1):
+        tasks.append(connect(tosend + str(i).rjust(8, '0')))
+    # await connect(str(0).rjust(8, '0'))
+
+    await asyncio.gather(*tasks)
+    print("done")
+
+
+# Run the `main()` function
+if __name__ == "__main__":
+    tosend = "nothing"
+
+    asyncio.run(main(tosend))
---
+++ ftp_server.py
@@ -0,0 +1,135 @@
+import os
+import asyncio
+import socket
+
+os.chdir('myfiles')
+
+INTERFACE, SPORT = 'localhost', 8080
+CHUNK = 100
+
+EOF = "EOF"
+
+serverPassword = "password"
+
+intro_message = "Hello! Welcome to my (lyakhovs) server! I'm majoring in CS\n"
+
+
+async def client_handling(reader, writer):
+    print("passwordStart\n")
+    for attempts in range(3):
+        passwordInput = "Enter Password"
+        writer.write(passwordInput.encode())
+        await writer.drain()
+
+        data_length_hex = await reader.readexactly(8)
+        # Then we convert it from hex to integer format that we can work with
+        data_length = int(data_length_hex, 16)
+
+        sent = await reader.read(data_length)
+        password = sent.decode()
+        print(password)
+        print(attempts)
+        if attempts >= 2 and password != serverPassword:
+            writer.write(b"Too many attempts. Closing connection")
+            await writer.drain()
+            writer.close()
+            await writer.wait_closed()
+            break
+        if password != serverPassword:
+            writer.write(b"Incorrect \n")
+            await writer.drain()
+            print("incorrect \n")
+        else:
+            writer.write(intro_message.encode())
+            await writer.drain()
+            print("correct \n")
+            break
+
+
+    # command handling
+    while True:
+
+        data_length_hex = await reader.readexactly(8)
+        # Then we convert it from hex to integer format that we can work with
+        data_length = int(data_length_hex, 16)
+        print("waiting for command")
+        sent = await reader.read(data_length)
+        print(sent.decode())
+        if sent.decode() == "list":
+            list = os.listdir(".")
+            files = " ".join(list) + "\n"
+            writer.write(files.encode())
+            await writer.drain()
+            print(files)
+        elif sent.decode().startswith("put"):
+            print(sent.decode()[4:])
+            await putFunc(reader, writer, sent.decode()[4:])
+        elif sent.decode().startswith("remove"):
+            if os.path.exists(sent.decode()[7:]):
+                os.remove(sent.decode()[7:])
+                writer.write(b"file removed")
+                await writer.drain()
+            else:
+                writer.write(b"NAK file does not exist")
+                await writer.drain()
+        elif sent.decode().startswith("get"):
+            if os.path.exists(sent.decode()[4:]):
+
+                writer.write(b"ACK")
+                await writer.drain()
+                await getFunc(reader, writer, sent.decode()[4:])
+
+            else:
+                writer.write(b"NAK file does not exist")
+                await writer.drain()
+        elif sent.decode().startswith("close"):
+            writer.close()
+            await writer.wait_closed()
+            break
+        else:
+            writer.write(b"NAK command does not exist")
+            await writer.drain()
+
+    return
+
+
+async def putFunc(reader, writer, name):
+
+    with open(name, 'wb') as f:
+        while True:
+            dataChunk = await reader.read(CHUNK)
+            print(dataChunk)
+            if not dataChunk or dataChunk.endswith(EOF.encode()):
+                f.write(dataChunk[:-3])
+                break
+            f.write(dataChunk)
+    f.close()
+    writer.write(b"Uploaded file")
+    await writer.drain()
+    return
+
+
+async def getFunc(reader, writer, name):
+    with open(name, 'rb') as f:
+        while dataChunk := f.read(CHUNK):
+            writer.write(dataChunk)
+            await writer.drain()
+        writer.write(EOF.encode())
+        await writer.drain()
+    f.close()
+
+    return
+
+
+async def main():
+    server = await asyncio.start_server(
+            client_handling,
+            INTERFACE, SPORT
+    )
+
+    async with server:
+        await server.serve_forever()
+
+# Run the `main()` function
+if __name__ == "__main__":
+    asyncio.run(main())
